799dadba3e832d3ab1cb2a3023af4ec33454966d
EgonRasmussen <ecr@eucsyd.dk>
2023-01-16 16:57:20 +0100

    ok

---
+++ CRUD_MVVM/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MVVM_INPC.Platforms.iOS;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
